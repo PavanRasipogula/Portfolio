614fb98b8cb19571b5490fbf61da455cd2443f2d
PavanRasipogula <justmailtopavankumar@gmail.com>
2025-04-20 03:02:47 +0530

    Second1

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -9,8 +9,7 @@ const Footer = () => {
       <p>
         © <span className="year">{year}</span>{" "}
         <span className="highlight">RASIPOGULA PAVAN KUMAR.</span> All Rights
-        Reserved. Developed by{" "}
-        <span className="end">RASIPOGULA PAVAN KUMAR❤ </span>
+        Reserved. Developed by <span className="end">PAVAN RASIPOGULA❤ </span>
       </p>
     </footer>
   );
