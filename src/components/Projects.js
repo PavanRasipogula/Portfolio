6c4f6c0d80c1823e56cd798b94b6913bcc109069
PavanRasipogula <justmailtopavankumar@gmail.com>
2025-04-27 17:16:54 +0530

    Styles

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -5,38 +5,65 @@ import { FaGithub, FaExternalLinkAlt } from "react-icons/fa";
 const projects = [
   {
     title: "Face Recognition Attendance System",
-    description: "Developed a smart attendance system using face recognition with OpenCV and machine learning.  Enabled real-time facial recognition for marking attendance, reducing manual errors and proxy.",
-    technologies: ["Python", "OpenCV", "Machine Learning", "Face Recognition", "Database","Git"],
+    description:
+      "Developed a smart attendance system using face recognition with OpenCV and machine learning.  Enabled real-time facial recognition for marking attendance, reducing manual errors and proxy.",
+    technologies: [
+      "Python",
+      "OpenCV",
+      "Machine Learning",
+      "Face Recognition",
+      "Database",
+      "Git",
+    ],
     github: "https://github.com/yourusername/face-attendance",
     website: "#",
   },
   {
     title: " Plant Leaf Disease Prediction",
-    description: "Built a web-based application to detect plant leaf diseases using image classification techniques.Utilized computer vision and a trained ML model to classify disease from uploaded leaf images.",
-    technologies: ["Python", "ML", "Computer Vision", "Image Processing","Git"],
+    description:
+      "Built a web-based application to detect plant leaf diseases using image classification techniques.Utilized computer vision and a trained ML model to classify disease from uploaded leaf images.",
+    technologies: [
+      "Python",
+      "ML",
+      "Computer Vision",
+      "Image Processing",
+      "Git",
+    ],
     github: "https://github.com/PavanRasipogula/Final-Pro",
     website: "#",
   },
   {
     title: "Online Library Management System",
-    description: "Designed and developed a web application to automate library management tasks.Allowed admin to manage books, issue/return logs, and student records via an interactive dashboard",
-    technologies: ["Java", "Spring Boot", "MySQL", "HTML", "CSS", "JavaScript","Git"],
+    description:
+      "Designed and developed a web application to automate library management tasks.Allowed admin to manage books, issue/return logs, and student records via an interactive dashboard",
+    technologies: [
+      "Python",
+      "Django",
+      "MySQL",
+      "HTML",
+      "CSS",
+      "JavaScript",
+      "Git",
+    ],
     github: "https://github.com/PavanRasipogula/O-L-M-S",
     website: "#",
   },
   {
     title: "Portfolio Website",
-    description: "Responsive portfolio with dark mode, smooth animations & project showcase.Built using React, JavaScript, and HTML with a modern Neumorphism design.Features interactive sections, clean UI, and fully optimized for all devices.",
+    description:
+      "Responsive portfolio with dark mode, smooth animations & project showcase.Built using React, JavaScript, and HTML with a modern Neumorphism design.Features interactive sections, clean UI, and fully optimized for all devices.",
     technologies: ["React", "JavaScript", "HTML", "CSS", "Git"],
-    github: "https://github.com/yourusername/portfolio",
-    website: "#",
+    github: "https://github.com/PavanRasipogula/portfolio",
+    website: "https://pavanrasipogula.vercel.app/",
   },
 ];
 
 const Projects = () => {
   return (
     <section className="projects" id="projects">
-      <h2 className="projects-heading">Some Things <span className="me">I've Built..</span></h2>
+      <h2 className="projects-heading">
+        Some Things <span className="me">I've Built..</span>
+      </h2>
       <div className="projects-grid">
         {projects.map((proj, index) => (
           <div className="project-card" key={index}>
